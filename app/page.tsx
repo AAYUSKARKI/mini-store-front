45a996fca921458b01787bb458d18bd6cd8b5ccb
aayus karki <karki.aayush2003@icloud.com>
2025-09-14 08:22:40 +0545

    added shadcn ui

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,9 @@
-
+import { Button } from "@/components/ui/button"
 
 export default function Home() {
   return (
-   <h1>Home</h1>
-  );
+    <div>
+      <Button className="cursor-pointer">Click me</Button>
+    </div>
+  )
 }
