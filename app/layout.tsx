ea05a2e5958ae4cf7049ec238fa941cf62e9642f
aayus karki <karki.aayush2003@icloud.com>
2025-09-15 18:32:30 +0545

    toast position changed

--- app/layout.tsx
+++ app/layout.tsx
@@ -30,7 +30,7 @@ export default function RootLayout({
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
         <ThemeProvider>
-          <Toaster />
+          <Toaster position="top-right" />
           {children}
         </ThemeProvider>
       </body>
