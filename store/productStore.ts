6a06d24454f44ccd81c6fdbdbc8e0fcd878c8c9e
aayus karki <karki.aayush2003@icloud.com>
2025-09-15 14:07:35 +0545

    added action to fetch product by id

--- store/productStore.ts
+++ store/productStore.ts
@@ -1,15 +1,18 @@
 import { create } from "zustand";
 import type { Product } from "@/types";
-import { fetchProducts } from "@/api/products";
+import { fetchProducts,fetchProduct } from "@/api/products";
 
 interface ProductState {
+    product: Product | null;
     products: Product[];
     loading: boolean;
     error: string | null;
     fetchProducts: () => Promise<void>;
+    fetchProduct: (id: number) => Promise<void>;
 }
 
 export const useProductStore = create<ProductState>((set) => ({
+    product: null,
     products: [],
     loading: false,
     error: null,
@@ -23,4 +26,13 @@ export const useProductStore = create<ProductState>((set) => ({
             set({ error: "Failed to fetch products", loading: false });
         }
     },
+    fetchProduct: async (id: number) => {
+        set({ loading: true, error: null });
+        try {
+            const product = await fetchProduct(id);
+            set({ product, loading: false });
+        } catch (error) {
+            set({ error: "Failed to fetch product", loading: false });
+        }
+    }
 }));
